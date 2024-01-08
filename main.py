17bca886efbd49b096561dbd68a47605ea0aa08a
Agnieszka Białecka <aga5467@gmail.com>
2024-01-08 16:38:45 +0100

    Dodanie Dywergencji Kullbacka-Leiblera

--- main.py
+++ main.py
@@ -15,24 +15,25 @@ import sys
 
 
 # Wgranie zestawu danych
-df = pd.read_csv('C:/Users/aga54/Desktop/Praca_inżynierska/onlinefraud.csv')
-data = df['amount']
+df = pd.read_csv('C:/Users/aga54/Desktop/Praca_inżynierska/avocado.csv')
+data = df['Total Volume']
 df.isnull().sum()
 
-# Wyliczenie watości oczekiwanej
+
+# Wartość oczekiwana (teoretyczna)
 total_rows = df.shape[0]    # zliczanie wierszy
 Benford = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
 expected_counts = np.array([round(p * total_rows / 100) for p in Benford])
 print("Warość oczekiwana:", expected_counts)
 
 
-# Wyliczenie watości mierzonej
+# Wartość mierzona (empiryczna)
 fd_data = data.astype(str).str[:1].astype(int)
 digit = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 observed_counts = np.array([fd_data.value_counts()[i] for i in digit])
 print("Warość mierzona:", observed_counts)
 
-#Rozkład mierzony
+#Rozkład empiryczny (mierzony)
 measured_distribution = (observed_counts/total_rows)*100
 # print(measured_distribution)
 
@@ -40,13 +41,13 @@ measured_distribution = (observed_counts/total_rows)*100
 # Obliczenia dla testu Chi-kwadrat
 chi2_stat = np.sum((measured_distribution - Benford)**2 / Benford)  # * total_rows ??
 
-# Stopnie swobody to liczba kategorii minus 1
+# Stopnień swobody
 degrees_of_freedom = len(digit) - 1
 
-# Wartość p (przy użyciu rozkładu chi-kwadrat)
+# Wartość p
 p_value = 1 - chi2.cdf(chi2_stat, degrees_of_freedom)
 
-# Wyświetlenie wyników testu Chi-kwadrat
+# Wyniki testu Chi-kwadrat
 print()
 print("Wyniki testu Chi-kwadrat:")
 print("Wartość statystyki Chi-kwadrat:", chi2_stat)
@@ -55,7 +56,12 @@ print("Wartość p:", p_value)
 
 # Średnie odchylenie bezwzględne
 mad = ((np.sum(abs(measured_distribution - Benford)))/9)
-print("Wyniki średniego odchylania bezwzględnego: ", mad)
+print("Średnie odchylanie bezwzględne: ", mad)
+
+
+# Dywergencja Kullbacka-Leiblera
+divKL = np.sum(measured_distribution * abs(np.log2(measured_distribution/Benford)))
+print("Dywergencja Kullbacka-Leiblera: ", divKL)
 
 
 # Rysowanie wykresu
